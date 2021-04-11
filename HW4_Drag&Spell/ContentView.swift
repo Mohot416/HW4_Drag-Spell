f81e902f9e86bc0f402bf68912532f36d50d7c49
mj0323 <jackjemmy72@gmail.com>
2021-04-11 15:04:57 +0800

    Initial Commit

---
+++ HW4_Drag&Spell/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW4_Drag&Spell
+//
+//  Created by User05 on 2021/4/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW4_Drag&Spell/HW4_Drag_SpellApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW4_Drag_SpellApp.swift
+//  HW4_Drag&Spell
+//
+//  Created by User05 on 2021/4/11.
+//
+
+import SwiftUI
+
+@main
+struct HW4_Drag_SpellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
