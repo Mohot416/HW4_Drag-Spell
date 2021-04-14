320fb41a9821943ea5a89026741d3d838ee998aa
mj0323 <jackjemmy72@gmail.com>
2021-04-14 16:06:39 +0800

    稍微練習一下

--- HW4_Drag&Spell/ContentView.swift
+++ HW4_Drag&Spell/ContentView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var offset = CGSize.zero
+    @State private var newPos = CGSize.zero
+    var dragGesture: some Gesture{
+        DragGesture().onChanged({value in
+            offset.width = newPos.width + value.translation.width
+            offset.height = newPos.height + value.translation.height
+        }).onEnded({value in newPos = offset})
+    }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Image("BackGround").resizable().scaledToFill().frame(width: 1000, height: 600, alignment: .center)
+            VStack{
+                //baguette
+                Image("法國麵包").resizable().scaledToFit().frame(width: 200, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                HStack{
+                    Text(frch_letters[1].letter).padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/).background(Color(.gray))
+                    Text(frch_letters[0].letter).padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/).background(Color(.gray))
+                    Text("g").padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/).background(Color(.gray))
+                    Text("u").padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/).background(Color(.gray))
+                    Text("e").padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/).background(Color(.gray))
+                    Text("t").padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/).background(Color(.gray))
+                    Text("t").padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/).background(Color(.gray))
+                    Text("e").padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/).background(Color(.gray))
+                }
+                
+            }
+        }
     }
 }
 
---
+++ HW4_Drag&Spell/File.swift
@@ -0,0 +1,52 @@
+//
+//  File.swift
+//  HW4_Drag&Spell
+//
+//  Created by User05 on 2021/4/11.
+//
+
+import Foundation
+
+struct FrenchLettersStruct {
+    var letter : String
+    var number : Int
+}
+//abcdefghijklmnopqrstuvwxyzèéçàâêîôû
+
+var frch_letters : [FrenchLettersStruct] = [
+    FrenchLettersStruct(letter: "a", number: 0),
+    FrenchLettersStruct(letter: "b", number: 1),
+    FrenchLettersStruct(letter: "c", number: 2),
+    FrenchLettersStruct(letter: "d", number: 3),
+    FrenchLettersStruct(letter: "e", number: 4),
+    FrenchLettersStruct(letter: "f", number: 5),
+    FrenchLettersStruct(letter: "g", number: 6),
+    FrenchLettersStruct(letter: "h", number: 7),
+    FrenchLettersStruct(letter: "i", number: 8),
+    FrenchLettersStruct(letter: "j", number: 9),
+    FrenchLettersStruct(letter: "k", number: 10),
+    FrenchLettersStruct(letter: "l", number: 11),
+    FrenchLettersStruct(letter: "m", number: 12),
+    FrenchLettersStruct(letter: "n", number: 13),
+    FrenchLettersStruct(letter: "o", number: 14),
+    FrenchLettersStruct(letter: "p", number: 15),
+    FrenchLettersStruct(letter: "q", number: 16),
+    FrenchLettersStruct(letter: "r", number: 17),
+    FrenchLettersStruct(letter: "s", number: 18),
+    FrenchLettersStruct(letter: "t", number: 19),
+    FrenchLettersStruct(letter: "u", number: 20),
+    FrenchLettersStruct(letter: "v", number: 21),
+    FrenchLettersStruct(letter: "w", number: 22),
+    FrenchLettersStruct(letter: "x", number: 23),
+    FrenchLettersStruct(letter: "y", number: 24),
+    FrenchLettersStruct(letter: "z", number: 25),
+    FrenchLettersStruct(letter: "è", number: 26),
+    FrenchLettersStruct(letter: "é", number: 27),
+    FrenchLettersStruct(letter: "ç", number: 28),
+    FrenchLettersStruct(letter: "à", number: 29),
+    FrenchLettersStruct(letter: "â", number: 30),
+    FrenchLettersStruct(letter: "ê", number: 31),
+    FrenchLettersStruct(letter: "î", number: 32),
+    FrenchLettersStruct(letter: "ô", number: 33),
+    FrenchLettersStruct(letter: "û", number: 34),
+]
